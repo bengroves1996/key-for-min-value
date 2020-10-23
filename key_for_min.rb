f3b9aabaf882aee67103732bfaab7d16fbab5241
bengroves <bengroves15@gmail.com>
2020-10-23 15:44:51 -0400

    commit tree 6844bd8cafb90f95832243f183c69755edc56440

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    bin1 = []
+    swap = name_hash.invert
+    if name_hash == {}
+        return nil
+    else 
+        name_hash.collect do |keys,values|
+            bin1.append(values)
+        end 
+    end 
+    nums_sort = bin1.sort()
+    
+    return swap[nums_sort[0]]
 end
